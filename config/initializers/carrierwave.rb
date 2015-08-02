942b8abd510981f01a96b02f190917bef5e45cce
kelechodesarrollo <kelecho90desarrollo@gmail.com>
2015-08-02 14:20:31 -0400

    ENV variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
+    :provider               => ENV["provider"],                        # required
     :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
     :aws_secret_access_key  => ENV["aws_secret_access_key"],                        # required
   }
