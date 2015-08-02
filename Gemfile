ac54283dcb87320aed0e38158d5232ec708c5413
kelechodesarrollo <kelecho90desarrollo@gmail.com>
2015-08-02 14:00:54 -0400

    Figaro Gem

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "mini_magick"
 gem 'carrierwave'
 gem "fog"
+gem 'figaro'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIVSSS3HUVLIWMAIQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"],                        # required
   }
-  config.fog_directory  = 'mercodental'                    # required
+  config.fog_directory  = ENV["fog_directory"]                    # required
 end
